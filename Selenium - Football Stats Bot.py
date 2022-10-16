ed868c66c42a26a4348cb5e6425254ce70d76d51
petitkitsune <109251840+petitkitsune@users.noreply.github.com>
2022-10-16 21:38:42 +0200

    Create Selenium - Football Stats Bot.py

---
+++ Selenium - Football Stats Bot.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import pandas as pd
+import time
+
+# Topics: Locate a button, select element within dropdown and extract data from a table
+
+# define the website to scrape and path where the chromediver is located
+
+website = 'https://www.adamchoi.co.uk/teamgoals/detailed'
+path = '/Users/noca7ran/Downloads/chromedriver' 
+
+# define 'driver' variable
+driver = webdriver.Chrome(path)
+# open Google Chrome with chromedriver
+driver.get(website)
+
+# locate a button
+all_matches_button = driver.find_element(By.XPATH, '//label[@analytics-event="All matches"]')
+# click on a button
+all_matches_button.click()
+
+# using the "box" section as a reference to help us locate an element inside
+box = driver.find_element(By.CLASS_NAME,'panel-body')
+# select dropdown and select element inside by visible text
+dropdown = Select(box.find_element(By.ID,'country'))
+dropdown.select_by_visible_text('Spain')
+# implicit wait (useful in JavaScript driven websites when elements need seconds to load and avoid error "ElementNotVisibleException")
+time.sleep(5)
+# select elements in the table
+matches = driver.find_elements(By.CSS_SELECTOR,'tr')
+
+# storage in a list
+all_matches = [match.text for match in matches]
+
+#quit drive we opened in the beginning
+driver.quit()
+
+# Bonus: Create Dataframe in Pandas and export to CSV (Excel)
+df = pd.DataFrame({'goals': all_matches})
+print(df)
+df.to_csv('tutorial.csv', index=False)
